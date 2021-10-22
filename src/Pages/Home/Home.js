aac9946f2515a5ac7db1ee30367894e79171af9c
mailmati420 <86460845+mailmati420@users.noreply.github.com>
2021-10-22 14:30:35 +0530

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -147,8 +147,8 @@ function Home() {
                         </div>
                         <div className="col-2 ">
                             <p>Coming Soon</p>
-                            <h1>Roll Up Originals</h1>
-                            <small >The extaordinary breathable hoddie which faishonable, trippy as well as good for your body. Will be available in both slim fit and and loose fit.</small>
+                            <h1>Blazeup Originals</h1>
+                            <small >The extaordinary breathable hoddie which is faishonable, trippy as well as good for your body. This will be available in both slim fit and and loose fit.</small>
                             <br />
                             <Link to="/Product" replace
                                 style={{
