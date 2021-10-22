53a5f71f1311f7ba3d5db64194da50de0dc041ac
sanidhya3340 <sanidhyabhardwaj3340@gmail.com>
2021-10-22 16:04:59 +0530

    made the form color changes

--- src/Components/ShippingForm/ShippingForm.js
+++ src/Components/ShippingForm/ShippingForm.js
@@ -33,80 +33,183 @@ function ShippingForm({data , total}) {
 
     
     return (
-        <div className="">
-            <article className="mw6 center br3 pa3 pa4-ns w-100 mv3 shadow-1"  >
-                <main class="pa4 black-80">
+      <div className="form-class">
+        <article className="mw6 center br3 pa3 pa4-ns w-100 mv3 shadow-1">
+          <main class="pa4 black-80">
+            {checker ? (
+              <form class="measure center w-100" onSubmit={sendEmail}>
+                <fieldset
+                  id="sign_up"
+                  className="ba b--transparent ph0 mh0 w-100"
+                >
+                  <legend className="f4 fw6 ph0 mh0" style={{ color: "white" }}>
+                    Shipping Form
+                  </legend>
+                  <div className="mt3">
+                    <label
+                      style={{ color: "white" }}
+                      className="db fw6 lh-copy f6"
+                      for="name"
+                    >
+                      Name
+                    </label>
+                    <input
+                      style={{ color: "white" }}
+                      className="pa2 input-reset ba bg-transparent w-100"
+                      type="name"
+                      name="name"
+                      id="name"
+                      required
+                    />
+                  </div>
+                  <div className="mt3">
+                    <label
+                      style={{ color: "white" }}
+                      className="db fw6 lh-copy f6"
+                      for="email-address"
+                    >
+                      Email
+                    </label>
+                    <input
+                      style={{ color: "white" }}
+                      className="pa2 input-reset ba bg-transparent w-100"
+                      type="email"
+                      name="email-address"
+                      id="email-address"
+                      required
+                    />
+                  </div>
+                  <div className="mt3">
+                    <label
+                      style={{ color: "white" }}
+                      className="db fw6 lh-copy f6"
+                      for="tel"
+                    >
+                      Ph-No
+                    </label>
+                    <input
+                      style={{ color: "white" }}
+                      className="pa2 input-reset ba bg-transparent w-100"
+                      type="tel"
+                      name="tel"
+                      id="tel"
+                      required
+                    />
+                  </div>
+                  <div className="mt3">
+                    <label
+                      style={{ color: "white" }}
+                      className="db fw6 lh-copy f6"
+                      for="address"
+                    >
+                      Shipping Address
+                    </label>
+                    <textarea
+                      style={{ color: "white" }}
+                      className="pa2 input-reset ba bg-transparent w-100"
+                      type="text"
+                      name="shipping-address"
+                      id="shipping-address"
+                      required
+                    />
+                  </div>
 
-                    {checker ? (
+                  <div className="mt3">
+                    <label
+                      style={{ color: "white" }}
+                      className="db fw6 lh-copy f6"
+                      for="pin-code"
+                    >
+                      Pin-Code
+                    </label>
+                    <input
+                      style={{ color: "white" }}
+                      className="pa2 input-reset ba bg-transparent w-100"
+                      type="pin"
+                      name="pin-code"
+                      id="pin-code"
+                      required
+                    />
+                  </div>
 
-                    <form class="measure center w-100" onSubmit={sendEmail}>
-                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0 w-100">
+                  <div className="mt3">
+                    <label
+                      style={{ color: "white" }}
+                      className="db fw6 lh-copy f6"
+                      for="product-name"
+                    >
+                      Product name
+                    </label>
+                    {data.map((curElem, index) => {
+                      return (
+                        <input
+                          style={{ color: "white" }}
+                          className="pa2 input-reset ba bg-transparent w-100"
+                          readonly="readonly"
+                          value={
+                            data[index].text +
+                            " (quantity : " +
+                            data[index].value +
+                            ")"
+                          }
+                          name="product-name"
+                          id="product-name"
+                        />
+                      );
+                    })}
+                  </div>
 
+                  <div className="mt3">
+                    <label
+                      style={{ color: "white" }}
+                      className="db fw6 lh-copy f6"
+                      for="total"
+                    >
+                      Amount
+                    </label>
+                    <input
+                      style={{ color: "white" }}
+                      className="pa2 input-reset ba bg-transparent w-100"
+                      readonly="readonly"
+                      value={total}
+                      name="total"
+                      id="total"
+                    />
+                  </div>
+                </fieldset>
+                <div>
+                  <input
+                    style={{ color: "white", backgroundColor: "#3146E9" }}
+                    className="btn formSubmit b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                    type="submit"
+                    value="Submit"
+                  />
+                </div>
+              </form>
+            ) : (
+              <div>
+                <h1 style={{ color: "white" }}>
+                  Your Order Is Done Now Complete The Payment Process To Get
+                  Your Order
+                </h1>
+                {/* <h3>Kindely Note Down Your Full Amount Sent To Your Mail</h3> */}
 
-                        
-                            <legend className="f4 fw6 ph0 mh0">Shipping Form</legend>
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" for="name">Name</label>
-                                <input className="pa2 input-reset ba bg-transparent w-100" type="name" name="name" id="name"  required/>
-                            </div>
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" for="email-address">Email</label>
-                                <input className="pa2 input-reset ba bg-transparent w-100" type="email" name="email-address" id="email-address" required/>
-                            </div>
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" for="tel">Ph-No</label>
-                                <input className="pa2 input-reset ba bg-transparent w-100" type="tel" name="tel" id="tel" required/>
-                            </div>
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" for="address">Shipping Address</label>
-                                <textarea className="pa2 input-reset ba bg-transparent w-100" type="text" name="shipping-address" id="shipping-address" required/>
-                            </div>
-
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" for="pin-code">Pin-Code</label>
-                                <input className="pa2 input-reset ba bg-transparent w-100" type="pin" name="pin-code" id="pin-code" required/>
-                            </div>
-
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" for="product-name">Product name</label>
-                                {
-                                    data.map((curElem , index) => {
-                                        return (
-                                            <input className="pa2 input-reset ba bg-transparent w-100" readonly="readonly"   value = {data[index].text + " (quantity : " +  data[index].value + ")"}   name="product-name" id="product-name" />
-                                        )
-                                    })
-                                }
-                               
-                            </div>
-
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" for="total">Amount</label>
-                                <input className="pa2 input-reset ba bg-transparent w-100" readonly="readonly"  value = {total}  name="total" id="total" />
-                            </div>
-                          
-                          
-                            
-                        </fieldset>
-                        <div >
-                            <input className="formSubmit b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Submit" />
-                        </div>
-
-                    </form>) :
-
-                    (
-                        <div>
-                            <h1>Your Order Is Done Now Complete The Payment Process To Get Your Order</h1>
-                            <h3>Kindely Note Down Your Full Amount Sent To Your Mail</h3>
-
-                            <a className="formSubmit b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" href={`https://pages.razorpay.com/pl_I865j338eSXjat/view?amount=${total}`} data-text="Pay Now" data-color="#528FF0" data-size="medium">Pay Now {total}</a>
-                        </div>
-                    )
-                    }
-                </main>
-            </article>
-            
-        </div>
-    )
+                <a
+                  className="formSubmit b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                  href={`https://pages.razorpay.com/pl_I865j338eSXjat/view?amount=${total}`}
+                  data-text="Pay Now"
+                  data-color="#528FF0"
+                  data-size="medium"
+                  style={{ backgroundColor: "#FFFFFF" }}
+                >
+                  Pay Now {total}
+                </a>
+              </div>
+            )}
+          </main>
+        </article>
+      </div>
+    );
 }
 
 export default ShippingForm
